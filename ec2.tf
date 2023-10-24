8bbb4f8fac60610bd6265e5fee4255e6c0af5b61
Rabiat Blessing Ibrahim <129818381+EseoheB@users.noreply.github.com>
2023-10-23 21:06:42 -0600

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 # Creating EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
-    ami                    = "ami-0df435f331839b2d6"
-  instance_type          = "t2.micro"
+    ami                    = var.ami_id
+  instance_type          = var.instance_type
+  associate_public_ip_address = var.enable_public_ip
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   subnet_id              = aws_subnet.public-subnet-1.id
   tags = {

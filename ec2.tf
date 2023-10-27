eca3f1c91654d4d78a9e5fd1c82f5698bf39b62c
Rabiat Blessing Ibrahim <129818381+EseoheB@users.noreply.github.com>
2023-10-26 22:06:40 -0600

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,10 @@
 # Creating EC2 instance in Public Subnet
 resource "aws_instance" "web_server" {
-    ami                    = var.ami_id
-  instance_type          = var.instance_type
+  count = var.env == "prod" ? 4 : 2
+  ami                    = "ami-0dbc3d7bc646e8516"
+  instance_type          = var.env == "prod" ? "t2.medium" : "t2.micro"
   associate_public_ip_address = var.enable_public_ip
   vpc_security_group_ids = [aws_security_group.web_server_sg.id]
   subnet_id              = aws_subnet.public-subnet-1.id
-  tags = {
-    Name = "Web-server"
-  }
+  tags = merge({Name = "${local.project_name}-${local.env}-instance-${count.index}"}, local.project_tags)
 }
